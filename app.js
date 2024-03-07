515a52a0613583c8c10129468a78f20620aabc4c
EnricoClem <enrico.clem94@gmail.com>
2024-03-07 18:40:44 +0100

    ADD - application class box elements and css style

--- app.js
+++ app.js
@@ -3,3 +3,31 @@
 // per i multipli di 5 stampi “Buzz” al post del numero.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 
+// Impostare il numero massimo su cui basare il programma e area di esecuzione su DOM
+const maxIterations = 100;
+const containerDomElement = document.getElementById('container');
+
+// Avviare la ricerca dei numeri da sostituire
+for (let number = 1; number <= maxIterations; number++) {
+
+    const boxDomElement = document.createElement('div');
+
+    let result = number;
+    let boxClass = 'box_number';
+
+    if (number % 3 === 0 && number % 5 === 0) {
+        result = 'FizzBuzz';
+        boxClass = 'box_fizzbuzz';
+    } else if (number % 3 === 0) {
+        result = 'Fizz';
+        boxClass = 'box_fizz';
+    } else if (number % 5 === 0) {
+        result = 'Buzz';
+        boxClass = 'box_buzz';
+    }
+
+    boxDomElement.classList.add('box', boxClass);
+    boxDomElement.innerHTML = result;
+    containerDomElement.append(boxDomElement);
+    
+}
